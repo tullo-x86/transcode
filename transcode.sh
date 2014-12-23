9d3de74726d993a78160eb8aadc4ea787c3e04d4
LuminousElement <d.tullemans@gmail.com>
2014-12-22 18:05:13 -0600

    Now makes M4A files, because PowerAmp doesn't support Opus yet :(

--- transcode.sh
+++ transcode.sh
@@ -4,6 +4,9 @@
 #
 # Transcodes FLAC to Opus in parallel
 
+# TODO: Copy tags
+# TODO: Test case-sensitivity of file extension
+
 E_BADPATH=62
 E_BADARGS=63
 E_MISSINGPROGRAM=64
@@ -12,12 +15,12 @@ CPU_CORES=4
 
 if [ ! -e /usr/bin/flac ] || [ ! -e /usr/bin/opusenc ]
 then
-    echo "You need opus-tools and flac installed"
+    echo "You need fdkaac-encoder and flac installed"
     exit $E_MISSING_PROGRAM
 fi
 
 print_usage() {
-  echo "\`$(basename $0)\` transcodes a directory structure of FLAC files to Opus files, keeping tags and structure"
+  echo "\`$(basename $0)\` transcodes a directory structure of FLAC files to M4A-encapsulated AAC files, keeping tags and structure"
   echo
   echo "Usage: $(basename $0) [library/root] filter ~/dest/dir bitrate"
   echo
@@ -25,10 +28,10 @@ print_usage() {
   echo "Filter will be passed to \`find\` as a DIRECTORY path filter, and .FLAC files in matched directories will be transcoded."
   echo "Dest dir is where transcoded music will be placed"
   echo
-  echo "Example: $(basename $0) ~/Music Visions ~/temp/transcode 140"
+  echo "Example: $(basename $0) ~/Music Visions ~/temp/transcode"
   echo
   echo "         In this case, '~/Music/Haken/Visions/02 Nocturnal Conspiracy.flac'"
-  echo "         will be transcoded to '~/temp/transcode/Haken/Visions/02 Nocturnal Conspiracy.opus'"
+  echo "         will be transcoded to '~/temp/transcode/Haken/Visions/02 Nocturnal Conspiracy.m4a'"
   echo
 }
 
@@ -37,18 +40,16 @@ fail_args() {
   exit $E_BADARGS
 }
 
-if [ $# -eq 3 ]
+if [ $# -eq 2 ]
   then
     libsrc_root=`pwd`
     filter=$1
     libdst_root=${2%/}
-    quality=$3
-elif [ $# -eq 4 ]
+elif [ $# -eq 3 ]
   then
     libsrc_root=${1%/}
     filter=$2
     libdst_root=${3%/}
-    quality=$4
 else
   fail_args
 fi
@@ -58,14 +59,19 @@ if [ ! -d "${libsrc_root}" ] || [ ! -d "${libdst_root}" ]
 fi
 
 c_libsrc_root=$( cd "${libsrc_root}" ; pwd -P )
-echo "Canonical library root is ${c_libsrc_root}"
+#echo "Canonical library root is ${c_libsrc_root}"
 c_libdst_root=$( cd "${libdst_root}" ; pwd -P )
-echo "Canonical destination root is ${c_libdst_root}"
+#echo "Canonical destination root is ${c_libdst_root}"
 
 cd $libsrc_root
 
 flac_files=$(find -L . -type f -path "*${filter}*/*" -name "*.flac")
 relative_paths=$( echo "$flac_files" | sed "s/\.\/\(.*\)\.flac/\1/g" )
+
+echo    "+--------------->"
+echo -n "| Files to transcode: "
+echo "${flac_files}" | wc -l
+echo    "+-------------------------->"
 
 # Create directory paths
 find -L . -type d -path "*${filter}*" -exec mkdir -p "${c_libdst_root}/{}" \;
@@ -74,13 +80,12 @@ find -L . -type d -path "*${filter}*" -exec mkdir -p "${c_libdst_root}/{}" \;
 export libsrc_root
 export c_libdst_root
 transcode_file() {
-  #echo $PWD
-  #echo "flac -cds  ./$1.flac  | opusenc --quiet --bitrate $2 -  $c_libdst_root/$1.opus"
-         flac -cds "./$1.flac" | opusenc --quiet --bitrate $2 - "$c_libdst_root/$1.opus"
+  #TODO: read tags
+  flac -cds "./$1.flac" | fdkaac -SI --profile 2 --bitrate-mode 5 --gapless-mode 2 -o ../out/test.m4a -
 }
 export -f transcode_file
 
-parallel -u -P $CPU_CORES --eta "transcode_file {} $quality" ::: "$relative_paths"
+parallel -u -P $CPU_CORES --eta "transcode_file {}" ::: "$relative_paths"
 
 echo
 echo "end of line."
