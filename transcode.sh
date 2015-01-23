4ac3708916d2509e0e77eb841fe5fbf507148f05
Daniel Tullemans <tully@be-lumino.us>
2015-01-23 11:58:50 -0600

    FFmpeg version copies metadata

--- transcode.sh
+++ transcode.sh
@@ -4,7 +4,21 @@
 #
 # Transcodes FLAC to Opus in parallel
 
-# TODO: Copy tags
+# Uses GNU Parallel
+# @article{Tange2011a,
+#  title = {GNU Parallel - The Command-Line Power Tool},
+#  author = {O. Tange},
+#  address = {Frederiksberg, Denmark},
+#  journal = {;login: The USENIX Magazine},
+#  month = {Feb},
+#  number = {1},
+#  volume = {36},
+#  url = {http://www.gnu.org/s/parallel},
+#  year = {2011},
+#  pages = {42-47}
+# }
+
+
 # TODO: Copy image files
 # TODO: Copy files which are already compressed in a lossy format
 # TODO: Test case-sensitivity of file extension
@@ -15,16 +29,16 @@ E_MISSINGPROGRAM=64
 
 CPU_CORES=4
 
-if [ ! -e /usr/bin/flac ] || [ ! -e /usr/bin/fdkaac ]
+if [ ! -e `which ffmpeg` ]
 then
-    echo "You need fdkaac-encoder and flac installed"
+    echo "You need ffmpeg installed."
     exit $E_MISSING_PROGRAM
 fi
 
 print_usage() {
   echo "\`$(basename $0)\` transcodes a directory structure of FLAC files to M4A-encapsulated AAC files, keeping tags and structure"
   echo
-  echo "Usage: $(basename $0) [library/root] filter ~/dest/dir bitrate"
+  echo "Usage: $(basename $0) [library/root] filter ~/dest/dir"
   echo
   echo "Library root is the root of your music library (transcoded dir structure will mirror relative paths from here). It will be recursed over, looking for FLAC files to transcode"
   echo "Filter will be passed to \`find\` as a DIRECTORY path filter, and .FLAC files in matched directories will be transcoded."
@@ -57,7 +71,9 @@ else
 fi
 
 if [ ! -d "${libsrc_root}" ] || [ ! -d "${libdst_root}" ]
-  then fail_args
+  then 
+    echo "Directory error"
+    fail_args
 fi
 
 c_libsrc_root=$( cd "${libsrc_root}" ; pwd -P )
@@ -65,7 +81,7 @@ c_libsrc_root=$( cd "${libsrc_root}" ; pwd -P )
 c_libdst_root=$( cd "${libdst_root}" ; pwd -P )
 #echo "Canonical destination root is ${c_libdst_root}"
 
-cd $libsrc_root
+cd "$libsrc_root"
 
 flac_files=$(find -L . -type f -path "*${filter}*/*" -name "*.flac")
 relative_paths=$( echo "$flac_files" | sed "s/\.\/\(.*\)\.flac/\1/g" )
@@ -78,12 +94,15 @@ echo    "+-------------------------->"
 # Create directory paths
 find -L . -type d -path "*${filter}*" -exec mkdir -p "${c_libdst_root}/{}" \;
 
+
+
 # Transcode files
 export libsrc_root
 export c_libdst_root
 transcode_file() {
   #TODO: read tags
-  flac -cds "./$1.flac" | fdkaac -SI --profile 2 --bitrate-mode 5 --gapless-mode 2 -o ../out/test.m4a -
+  #flac -cds "./$1.flac" | fdkaac -SI --profile 2 --bitrate-mode 5 --gapless-mode 2 -o "$c_libdst_root/$1.m4a" -
+  ffmpeg -y -loglevel error -i "./$1.flac" -vn -c:a libfdk_aac -vbr 4 "$c_libdst_root/$1.m4a"
 }
 export -f transcode_file
 
